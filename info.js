4d74be01755c33f3ecb95acd3a2074457251e7d9
jahime001 <jahimecamea@gmail.com>
2022-11-21 16:27:00 -0500

    resume

---
+++ info.js
@@ -0,0 +1,15 @@
+const resume = {
+    "name": "Jahime Cameau",
+        "website": "https://jahime001.github.io/",
+            "linkedin": "https://www.linkedin.com/in/tomatohammado/",
+                "github": "jahime001",
+                    "email": "jahimecameau@gmail.com",
+                        "resume": null,
+                            "proficientTechnologies": [
+                                "Javascript",
+                                "React",
+                            ],
+                                "otherSkills": [
+                                    "Pc Building"
+                                ]
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+## npm-resume
+
+Resume made using npm
